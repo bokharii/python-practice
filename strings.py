eb53e81f7d69d444ac78e282a3ff8582864c79ea
sbokhari <bokharisaab12345@gmail.com>
2026-07-19 21:32:24 -0400

    string methods

--- strings.py
+++ strings.py
@@ -1,6 +1,28 @@
-print("hello world")
+# print("hello world")
 
-m = "yo"
+# m = """hi
+# my name is "saaquib bokhari" whats
+# up"""
 
-print(m)
+message = "Hello World!"
+print(len(message))
 
+# print(m[0:2])
+print(message[:5])
+
+### string methods
+print(message.lower())  # lowercase
+print(message.upper())  # uppercase
+
+print(message.count("l"))  # counts how many times something appears in a string
+
+print(
+    message.find("l")
+)  # finds the first index of where the character/word appears in a string
+print(message.find("p"))  # returns -1 if it doesn't exist in the string
+
+# replaces the first argument with the second argument.
+new_message = message.replace(
+    "World", "Universe"
+)  # needs to be saved to a new variable since the replacement does not happen in-place
+print(new_message)
