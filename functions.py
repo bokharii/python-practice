3e169cd3e2b18488acfca7faf4ae0b3b7e93a0d7
sbokhari <bokharisaab12345@gmail.com>
2026-07-22 22:59:55 -0400

    functions in python

---
+++ functions.py
@@ -0,0 +1,11 @@
+# ### functions defined using def
+# def hello_func():
+#   pass # syntactic placeholder that literally does nothing when executed. useful for when you want to sketch out your fn
+
+# print(hello_func) # need () to execute the fn
+
+def hello_func():
+  print('Hello Function!')
+
+hello_func()
+
